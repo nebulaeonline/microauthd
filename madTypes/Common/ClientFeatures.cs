0fa9121ea72bbcc470a98decdf49dc343eb7180a
Rob Horvath <47482564+robhorvath3@users.noreply.github.com>
2025-07-07 14:38:46 -0400

    Begin prep for session-based auth

--- madTypes/Common/ClientFeatures.cs
+++ madTypes/Common/ClientFeatures.cs
@@ -10,6 +10,8 @@ public static class ClientFeatures
         TotpIssuer = 1,
         TokenExpiration = 2,
         RefreshTokenExpiration = 3,
+        SessionBasedAuth = 4,
+        SessionBasedMaxAge = 5,
     }
 
     public record FeatureFlagMetadata(ClientFeatures.Flags Flag, string FlagString, string Description, bool HasExtendedOptions);
@@ -19,7 +21,9 @@ public static class ClientFeatures
         { Flags.EnableTotp, "ENABLE_TOTP" },
         { Flags.TotpIssuer, "TOTP_ISSUER" },
         { Flags.TokenExpiration, "TOKEN_EXPIRATION" },
-        { Flags.RefreshTokenExpiration, "REFRESH_TOKEN_EXPIRATION" }
+        { Flags.RefreshTokenExpiration, "REFRESH_TOKEN_EXPIRATION" },
+        { Flags.SessionBasedAuth, "SESSION_BASED_AUTH" },
+        { Flags.SessionBasedMaxAge, "SESSION_BASED_MAX_AGE" }
     };
 
     private readonly static Dictionary<Flags, string> _descriptions = new()
@@ -27,7 +31,9 @@ public static class ClientFeatures
         { Flags.EnableTotp, "Enable TOTP authentication" },
         { Flags.TotpIssuer, "Issuer for TOTP apps" },
         { Flags.TokenExpiration, "Expiration time for access tokens (in seconds)" },
-        { Flags.RefreshTokenExpiration, "Expiration time for refresh tokens (in seconds)" }
+        { Flags.RefreshTokenExpiration, "Expiration time for refresh tokens (in seconds)" },
+        { Flags.SessionBasedAuth, "Enable session-based authentication (single-token per session, built-in login pages)" },
+        { Flags.SessionBasedMaxAge, "Maximum age for session-based authentication (in seconds)" }
     };
 
     private readonly static Dictionary<Flags, bool> _hasExtendedOptions = new()
@@ -35,7 +41,9 @@ public static class ClientFeatures
         { Flags.EnableTotp, false },
         { Flags.TotpIssuer, true },
         { Flags.TokenExpiration, true },
-        { Flags.RefreshTokenExpiration, true }
+        { Flags.RefreshTokenExpiration, true },
+        { Flags.SessionBasedAuth, false },
+        { Flags.SessionBasedMaxAge, true   }
     };
 
     public static string GetFlagString(Flags flag) =>
